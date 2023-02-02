6d09c23dbad88b04ce6e80775171c4b8da9f22af
Miguel Buitrago <mb.dev@lanacionaldelicores.com>
2023-02-02 16:55:01 -0500

    Added error view

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-7">
+    <p>{{ error.statusCode }}</p>
+    <p>Opps...</p>
+    <p>{{ error.message }}</p>
+    <v-btn @click="handleClearError">Go Home...</v-btn>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"]);
+const handleClearError = () => clearError({redirect:'/'})
+</script>
+
+<style scoped></style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -12,9 +12,14 @@
 </template>
 
 <script setup>
+import { create } from 'domain';
+
 const { id } = useRoute().params;
 const uri = "https://fakestoreapi.com/products/" + id;
 const { pending, data: product } = await useLazyFetch(uri, { key: id });
+if(!product.value && !pending){
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+}
 </script>
 
 <style lang="scss" scoped></style>
